07a37221b6ab9a6b926f54f23e3e878b6409a67d
unknown <okicandra21@gmail.com>
2017-01-31 10:12:03 +0700

    Java class barang

--- JavaClass/app/src/main/java/oki/candra/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/oki/candra/javaclass/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     public String showString;
     public TextView showText;
+    public Barang []arrBarang = new Barang[10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +17,32 @@ public class MainActivity extends AppCompatActivity {
         showText = (TextView) findViewById(R.id.show_text);
         showString = "Manipulasi class Java Android";
         addSeparator();
+
+
+        initBarang();
+        showString += arrBarang[5].toString();
+        addSeparator();
+        showString += arrBarang[0].toString();
+        addSeparator();
         showText.setText(showString);
     }
 
     public void addSeparator()
     {
         showString += "\n------------------------------------------\n";
+    }
+
+    public void initBarang()
+    {
+        arrBarang [0] = new Barang("Laptop",1,7000000);
+        arrBarang [1] = new Barang("Printer",Barang.ELEKTRONIK,500000);
+        arrBarang [2] = new Barang("Monitor",Barang.ELEKTRONIK,600000);
+        arrBarang [3] = new Barang("Scanner",Barang.ELEKTRONIK,600000);
+        arrBarang [4] = new Barang("Meja",Barang.NON_ELEKTRONIK,125000);
+        arrBarang [5] = new Barang("Kursi",Barang.NON_ELEKTRONIK,60000);
+        arrBarang [6] = new Barang("Lemari",Barang.NON_ELEKTRONIK,400000);
+        arrBarang [7] = new Barang("Handphone",Barang.ELEKTRONIK,1800000);
+        arrBarang [8] = new Barang("Softcase",Barang.NON_ELEKTRONIK,150000);
+        arrBarang [9] = new Barang("Sound System",Barang.ELEKTRONIK,1500000);
     }
 }
---
+++ JavaClass/app/src/main/java/oki/candra/javaclass/Transaksi.java
@@ -0,0 +1,7 @@
+package oki.candra.javaclass;
+
+/**
+ * Created by D2J-00 on 31/01/2017.
+ */
+public class Transaksi {
+}
