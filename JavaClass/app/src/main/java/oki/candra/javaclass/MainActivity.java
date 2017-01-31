9152619403d25c06aecdecf9490fd09b649362d3
unknown <okicandra21@gmail.com>
2017-01-31 09:11:35 +0700

    Tugas Pertama

--- JavaClass/app/src/main/java/oki/candra/javaclass/MainActivity.java
+++ JavaClass/app/src/main/java/oki/candra/javaclass/MainActivity.java
@@ -2,12 +2,25 @@ package oki.candra.javaclass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public String showString;
+    public TextView showText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showText = (TextView) findViewById(R.id.show_text);
+        showString = "Manipulasi class Java Android";
+        addSeparator();
+        showText.setText(showString);
+    }
+
+    public void addSeparator()
+    {
+        showString += "\n------------------------------------------\n";
     }
 }
