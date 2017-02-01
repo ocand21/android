475bd56a686b9b7d04f112b34f54a47a101ebee1
ocand <okicandra21@gmail.com>
2017-02-01 08:09:04 +0700

    Java Class

--- JavaClass/app/src/main/java/oki/candra/javaclass/Transaksi.java
+++ JavaClass/app/src/main/java/oki/candra/javaclass/Transaksi.java
@@ -1,7 +1,45 @@
 package oki.candra.javaclass;
 
+import java.util.ArrayList;
+
 /**
  * Created by D2J-00 on 31/01/2017.
  */
 public class Transaksi {
+
+    private ArrayList<Barang> ListBarang = new ArrayList<>();
+
+    public void addBarang(Barang barang)
+    {
+        this.ListBarang.add(barang);
+    }
+
+    public int totalTransaksi()
+    {
+        int hasil=0;
+        for(int i = 0; i < ListBarang.size();i++)
+        {
+            hasil += ListBarang.get(i).getHarga();
+        }
+
+        return hasil;
+    }
+
+    public String printTransaksi()
+    {
+        String text = "Barang yang dibeli pada transaksi ini adalah: \n";
+        text += "\n----------------------------------------------\n";
+
+        for(int i=0; i<ListBarang.size();i++)
+        {
+            text += ListBarang.get(i).toString();
+        }
+        text += "\n------------------------------------------------\n";
+        text += " Total pembelian: "+totalTransaksi()+"\n";
+        text += "\n------------------------------------------------\n";
+
+        return text;
+
+    }
+
 }
